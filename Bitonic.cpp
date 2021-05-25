4cefa4d12cb3776767b19f7f98af45cdc1170867
Jhanvi29 <65902206+Jhanvi29@users.noreply.github.com>
2021-05-25 23:02:14 +0530

    Update Bitonic.cpp

--- Bitonic.cpp
+++ Bitonic.cpp
@@ -43,41 +43,54 @@ Sample Output 3:
     lis[i] ==> Longest Increasing subsequence ending with arr[i]
     lds[i] ==> Longest decreasing subsequence starting with arr[i]
 */
-int lbs( int arr[], int n )
+int longestBitonicSequence(vector<int> &ARR, int N) 
 {
-   int i, j;
- 
-   /* Allocate memory for LIS[] and initialize LIS values as 1 for
-      all indexes */
-   int *lis = new int[n];
-   for (i = 0; i < n; i++)
-      lis[i] = 1;
- 
-   /* Compute LIS values from left to right */
-   for (i = 1; i < n; i++)
-      for (j = 0; j < i; j++)
-         if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
-            lis[i] = lis[j] + 1;
- 
-   /* Allocate memory for lds and initialize LDS values for
-      all indexes */
-   int *lds = new int [n];
-   for (i = 0; i < n; i++)
-      lds[i] = 1;
- 
-   /* Compute LDS values from right to left */
-   for (i = n-2; i >= 0; i--)
-      for (j = n-1; j > i; j--)
-         if (arr[i] > arr[j] && lds[i] < lds[j] + 1)
-            lds[i] = lds[j] + 1;
- 
- 
-   /* Return the maximum value of lis[i] + lds[i] - 1*/
-   int max = lis[0] + lds[0] - 1;
-   for (i = 1; i < n; i++)
-     if (lis[i] + lds[i] - 1 > max)
-         max = lis[i] + lds[i] - 1;
-   return max;
+	/*
+		LIS[i] stores the length of longest increasing sequence  
+		ending at index i.
+	*/
+
+	/*
+		LDS[i] stores the length of the longest decreasing sequence  
+		starting at index i.
+	*/
+	
+    int LIS[N], LDS[N];
+
+    for(int i = 0; i < N; i++)
+    {
+        LIS[i] = 1;
+
+        for(int j = 0; j < i; j++)
+        {
+            if(ARR[i] >= ARR[j])
+            {
+                LIS[i] = max(LIS[i], LIS[j] + 1);
+            }
+        }
+    }
+
+    for(int i = N - 1; i >= 0; i--)
+    {
+        LDS[i] = 1;
+
+        for(int j = i + 1; j < N; j++)
+        {
+            if(ARR[i] >= ARR[j])
+            {
+                LDS[i] = max(LDS[i], LDS[j] + 1);
+            }
+        }
+    }
+
+    int maxLength = 0;
+
+    for(int i = 0; i < N; i++)
+    {
+        maxLength = max(maxLength, LDS[i] + LIS[i] - 1);
+    }
+
+    return maxLength;    
 }
  
 /* Driver program to test above function */
